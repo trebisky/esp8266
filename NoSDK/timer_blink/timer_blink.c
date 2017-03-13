52c61eb0300a56cb87116d9858c2d1f0d653b994
Tom Trebisky <tom@mmto.org>
2017-03-12 21:39:55 -0700

    Add timer_blink for bare metal

--- NoSDK/timer_blink/timer_blink.c
+++ NoSDK/timer_blink/timer_blink.c
@@ -1,8 +1,11 @@
 /* ESP8266 "bare metal" interrupts.
  *
- * This uses timer interrupts to print messages every second.
+ * This uses timer interrupts to blink an LED
  *
- * It compiles to 528 bytes.
+ * For fun we do direct hardware access for the gpio
+ * as well as the pin mux.
+ *
+ * It compiles to 576 bytes.
  *
  * Tom Trebisky  3-12-2016
  */
@@ -51,22 +54,83 @@ struct dport {
 
 /* --------------------------------- */
 
+struct gpio {
+	volatile unsigned int	out;		/* 00 */
+	volatile unsigned int	out_set;	/* 04 */
+	volatile unsigned int	out_clear;	/* 08 */
+	volatile unsigned int	enable;		/* 0c */
+	volatile unsigned int	ena_set;	/* 10 */
+	volatile unsigned int	ena_clear;	/* 14 */
+	volatile unsigned int	in;		/* 18 */
+	volatile unsigned int	status;		/* 1c */
+	volatile unsigned int	status_set;	/* 20 */
+	volatile unsigned int	status_clear;	/* 24 */
+};
+
+#define GPIO_BASE (struct gpio *) 0x60000300;
+
+struct mux {
+	volatile unsigned int	conf;		/* 00 */
+	volatile unsigned int	gpio12;		/* 04 */
+	volatile unsigned int	gpio13;		/* 08 */
+	volatile unsigned int	gpio14;		/* 0C */
+	volatile unsigned int	gpio15;		/* 10 */
+	volatile unsigned int	gpio3;		/* 14 */
+	volatile unsigned int	txd;		/* 18 */
+	volatile unsigned int	sd_clk;		/* 1C */
+	volatile unsigned int	sd_d0;		/* 20 */
+	volatile unsigned int	sd_d1;		/* 24 */
+	volatile unsigned int	sd_d2;		/* 28 */
+	volatile unsigned int	sd_d3;		/* 2C */
+	volatile unsigned int	sd_cmd;		/* 30 */
+	volatile unsigned int	gpio0;		/* 34 */
+	volatile unsigned int	gpio2;		/* 38 */
+	volatile unsigned int	gpio4;		/* 3c */
+	volatile unsigned int	gpio5;		/* 40 */
+};
+
+/* The function mask is shifted over 4 bits and has a 2 bit "hole"
+ * in the middle (those bits are used to pullup control).
+ */
+#define FUNC_MASK	0x130
+
+/* I would rather not use this, but just generate constants with the
+ * necessary hole.
+ */
+#define funk(x)		(((x&4)<<2|(x&0x3))<<4)
+
+/* set to enable pullup */
+#define PULLUP_MASK	0x080
+
+#define MUX_BASE (struct mux *) 0x60000800;
+
+
+/* ---------- */
+
+#define LED_BIT		2	/* LED is on GPIO 2 */
+#define LED_MASK	0x04	/* 1 << 2 */
+
+/* --------------------------------- */
+
 /* Initializing this to zero here does not work.
  * My code would have to include some code to clear
  * the bss area.  Setting a non-zero initial value
  * works just fine.  No big surprise here.
  */
-int count = 1;
+int state = 1;	/* 1 is high is off */
 
 void
 timer_isr ( char *msg )
 {
-	if ( count < 100 )
-	    ets_printf ( "Tick %d: %s\n", count++, msg );
-}
+	struct gpio *gp = GPIO_BASE;
 
-/* Just something to test passing a pointer to the isr attach routine */
-char *bird = "duck";
+	if ( state )
+	    gp->out_clear = LED_MASK;
+	else
+	    gp->out_set = LED_MASK;
+
+	state = 1 - state;
+}
 
 /* Call with rate in microseconds */
 void
@@ -76,7 +140,7 @@ timer_setup ( int rate )
 	struct dport *dp = DPORT_BASE;
 
 	tp->ctrl = TC_ENABLE | TC_AUTO_LOAD | TC_DIV_16 | TC_EDGE;
-	ets_isr_attach ( TIMER_INUM, timer_isr, bird );
+	ets_isr_attach ( TIMER_INUM, timer_isr, 0 );
 
 	dp->edge |= 0x02;
 	ets_isr_unmask ( 1 << TIMER_INUM );
@@ -95,8 +159,28 @@ timer_setup ( int rate )
 	 */
 }
 
+/* clearing the LED bit will turn the LED on,
+ * so we set it to turn the LED off.
+ */
+void
+led_setup ( void )
+{
+	struct gpio *gp = GPIO_BASE;
+	struct mux *mp = MUX_BASE;
+
+	// PIN_FUNC_SELECT ( PERIPHS_IO_MUX_GPIO2_U, 0 );
+	// Just clear func bits to set zero
+	mp->gpio2 &= ~FUNC_MASK;
+
+	gp->ena_set = LED_MASK;
+
+	gp->out_set = LED_MASK;
+}
+
 /* 1 second In microseconds */
-#define TIMER_RATE	1000000
+// #define TIMER_RATE	1000000
+/* half second */
+#define TIMER_RATE	500000
 
 void
 call_user_start ( void )
@@ -104,6 +188,7 @@ call_user_start ( void )
     uart_div_modify(0, CLK_FREQ / 115200);
     ets_delay_us ( 1000 * 500 );
 
+    led_setup ();
     timer_setup ( TIMER_RATE );
 
     ets_printf("\n");
