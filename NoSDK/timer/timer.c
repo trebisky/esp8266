e2e51deb7653945ffdc31025ac8d5d32dd968fbf
Tom Trebisky <tom@mmto.org>
2017-03-13 14:28:12 -0700

    Tidy up timer load logic

--- NoSDK/timer/timer.c
+++ NoSDK/timer/timer.c
@@ -12,8 +12,31 @@
 /* The clock really is running at 52 Mhz when we
  * come out of the boot rom
  */
-// #define  CLK_FREQ   80*1000000
+#ifdef notdef
+/* Note that 80/1 = 80, 80/16 = 5, and 80/256 = 0.3125
+ * So, in the case when the prescaler is set to divide by 16,
+ *  we can just calculate the load value for the timer as
+ *  load = P*5 where P is the desired timer period in microseconds.
+ * Note that this overflows when a period longer
+ *  than 858 seconds is requested.
+ * We set "TIMER_TICKER" to this value times 100 to allow
+ *  for the 52 Mhz case below (this limits us to 8.58 second periods)
+ */
+#define  CLK_FREQ   80*1000000
+#define TIMER_TICKER	500
+#endif
+
+/* Note that 52/1 = 52, 52/16 = 3.25, and 52/256 = 0.203125
+ * So, in the case when the prescaler is set to divide by 16,
+ * if we are given a clock period in microsecons (call it P)
+ * We can calculate the load value for the timer as
+ * load = P*325/100
+ * This calculation will overflow a 32 bit unsigned when
+ *  P*325 = 2^32, i.e. when P = 13,215,284, i.e.
+ *  when a period of over 13 seconds is requested.
+ */
 #define  CLK_FREQ       (52*1000000)
+#define TIMER_TICKER	325
 
 struct timer {
 	volatile unsigned int	load;
@@ -81,18 +104,18 @@ timer_setup ( int rate )
 	dp->edge |= 0x02;
 	ets_isr_unmask ( 1 << TIMER_INUM );
 
-	/* This avoids overflow with a 32 bit unsigned int.
+	tp->load = (rate * TIMER_TICKER) / 100;
+
+#ifdef notdef
+	/* Here is the odd logic used in the SDK examples.
+	 * IT avoids overflow with a 32 bit unsigned int.
 	 * The magic number 858 is based on a 80 Mhz clock.
 	 */
 	if ( rate <= 858 )
 	    tp->load = (rate * (CLK_FREQ>>4)) / 1000000;
 	else
 	    tp->load = (rate>>2) * ((CLK_FREQ>>4)/250000) + (rate&0x3) * ((CLK_FREQ>>4)/1000000);
-
-	/* note in the above, if we ask for a 1 second delay (1000000 us),
-	 * we set the load register to CLK_FREQ/16
-	 * So with the prescaler set to /16, the timer counts at CPU_HZ / 16.
-	 */
+#endif
 }
 
 /* 1 second In microseconds */
